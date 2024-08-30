30abb5e192ddca3d85c5b28c697134858472f2aa
Halo1719 <rebeccamodise290@gmail.com>
2024-08-30 22:16:36 +0200

    added the weather API

--- src/script.js
+++ src/script.js
@@ -1,8 +1,27 @@
+function displayWeather(response);{
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city);{
+let apiKey = "2o31fcad3982f949ea00f9371b440tca";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayWeather);
+}
+
+
+
 function searcher(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+
+   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");

1c77b1b682ba53782bc3664b718e4fb804a37499
Halo1719 <rebeccamodise290@gmail.com>
2024-08-30 23:42:26 +0200

    added the description data

--- src/script.js
+++ src/script.js
@@ -1,28 +1,28 @@
-function displayWeather(response);{
+function displayWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
 
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
-function searchCity(city);{
-let apiKey = "2o31fcad3982f949ea00f9371b440tca";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayWeather);
+function searchCity(city) {
+  let apiKey = "2o31fcad3982f949ea00f9371b440tca";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayWeather);
 }
 
-
-
-function searcher(event) {
+function searcherKey(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
 
-   searchCity(searchInput.value);
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searcher);
+searchFormElement.addEventListener("submit", searcherKey);
+
+searchCity("Paris");
